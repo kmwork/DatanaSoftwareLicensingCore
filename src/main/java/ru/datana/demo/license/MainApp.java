9a9edc5e80aa1be9719e2370521fb377dee3d586
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-25 20:33:51 +0300

    проба

--- src/main/java/ru/datana/demo/license/MainApp.java
+++ src/main/java/ru/datana/demo/license/MainApp.java
@@ -1,8 +1,22 @@
 package ru.datana.demo.license;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class MainApp {
 
-    public static void main(String[] args) {
-       System.out.println("sing");
-    }
+    public static void main(String[] args) throws IOException {
+        JarFile jar = new JarFile(new File("/home/lin/work-lanit/DatanaSoftwareLicensingCore/target/DatanaSoftwareLicensingCore-001.jar"), true);
+        JarEntry jarEntry = jar.getJarEntry("u/datana/demo/license/MainApp.class");
+        System.out.println("Cert: "+Arrays.toString(jarEntry.getCertificates()));
+
+        System.out.println("Manifest:" + jar.getManifest());
+        System.out.println("sing");
+}
 }
